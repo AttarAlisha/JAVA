9d912c337c3bc89247a68bba452f2281a175bc43
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-07-10 22:31:18 -0700

    Create Student.java
    Java program that display Student information.

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student{
+
+public static void main(String args[])
+{
+	System.out.println("Name: Alisha Attar");
+	System.out.println("Age: 20");
+	System.out.println("Mob no.: 4387290123");
+	System.out.println("Mail: attaralisha@gmail.com");
+	System.out.println("Address: Ichalkarnji");
+	System.out.println("CGPA: 8.5");
+	System.out.println("Class : TY CSE(AI)");
+	
+}
+
+}
