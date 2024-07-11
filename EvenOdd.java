8c50ba23cee59f07868e7a4348c1f47a4bba7f23
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-07-10 22:28:22 -0700

    Create EvenOdd.java
    Java program for finding even and odd numbers.

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+class EvenOdd{
+
+public static void main(String args[])
+{
+	int a[]={4,6,2,8,1,9};
+	for(int i=0; i<=5;i++)
+	{
+		if(a[i]%2==0)
+		{
+			System.out.println("Even number");
+		}
+		else{
+			System.out.println("Odd number");
+		}
+	}
+}
+
+}
