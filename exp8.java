1d3f4755376108ef6c6206801b89e0d50e16b148
Alisha Attar <159872170+AttarAlisha@users.noreply.github.com>
2024-08-21 22:17:05 -0700

    Create exp8.java
    simple program to implement exception handling.

---
+++ exp8.java
@@ -0,0 +1,24 @@
+
+public class exp8
+
+{
+
+    public static void main(String args[])
+    {
+        System.out.println("program is started");
+        int a=10,b=0,c;
+
+        try{
+            c=a/b;
+            System.out.println(c);
+        }
+
+        catch(Exception e){
+                System.out.println("Cannot divided by 0");
+        }
+
+    System.out.println("Program end");
+
+    }
+
+}
